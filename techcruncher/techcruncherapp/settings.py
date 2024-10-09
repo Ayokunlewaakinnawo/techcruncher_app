fb059ee591b8c5891a1569960e82128614b634fd
Ayo Akinnawo <ayo@mramirez.vtc.voxtechnologies.com>
2024-10-09 09:37:59 -0500

    db_update

--- techcruncher/techcruncherapp/settings.py
+++ techcruncher/techcruncherapp/settings.py
@@ -88,24 +88,24 @@ WSGI_APPLICATION = 'techcruncherapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'techcruncher_postgresql',
-#        'USER': 'admin',
-#        'PASSWORD': 'admin',
-#        'HOST': 'postgres',
-#        'PORT': '5432',  # Assuming the default PostgreSQL port
-#    }
-#}
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'techcruncher_postgresql',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'postgres',
+        'PORT': '5432',  # Assuming the default PostgreSQL port
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
